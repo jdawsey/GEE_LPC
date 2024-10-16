e4cb0c80f391734e994588da666b5b04d139483a
jdawsey <91396339+jdawsey@users.noreply.github.com>
2024-10-16 01:39:06 -0500

    Adjusted image stack builder to send to drive

--- python_scripts/env_data_acq/gee_data_acq_improvements/package_formatting/env_data_acq_func/imagery_download.py
+++ python_scripts/env_data_acq/gee_data_acq_improvements/package_formatting/env_data_acq_func/imagery_download.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import geemap
+import ee
 
 """
 Download imagery per the area of all the shapefiles in a folder.
@@ -10,8 +11,8 @@ data_folder = the folder that the imagery will download to.
 image_stack = the image with all the desired bands to be downloaded.
 shp_dir = the directory address to the folder containing each of the shapefiles.
 """
-def imagery_download(folder_directory, data_folder, image_stack, shp_dir, 
-                     scale = 30):
+def imagery_download(folder_directory, data_folder, image_stack, shp_dir, drive = False,
+                     scale = 30, crs = 'EPSG:6350', max_pixels = 100000000):
     # cleaning out any extra files in case ran previously
     delete_file_path = f'{shp_dir}/'
     delete_file_paths = os.listdir(delete_file_path)
@@ -31,16 +32,45 @@ def imagery_download(folder_directory, data_folder, image_stack, shp_dir,
         if path.endswith('.shp'):
             count += 1
     print(f'Downloading data for {count} files')
+
+    if drive == True:
+        index_num = -1
+        for shp_file in shp_files:
+            index_num = index_num + 1
+            file_name_split = shp_file.split(".")
+            
+            shp_path = os.path.join(shp_dir, shp_file)
+            feature_collection = geemap.shp_to_ee(shp_path)
+            feature = feature_collection.geometry()
+
+            image_float = image_stack.toFloat()
     
-    index_num = -1
-    for shp_file in shp_files:
-        index_num = index_num + 1
-        file_name_split = shp_file.split(".")
-        
-        shp_path = os.path.join(shp_dir, shp_file)
-        feature_collection = geemap.shp_to_ee(shp_path)
-        feature = feature_collection.geometry()
-        image_path = f'{folder_directory}/{data_folder}/{file_name_split[0]}_env_image.tif'
-        geemap.ee_export_image(image_stack, image_path, scale, region = feature)
-        print(f'Export task started for {shp_file}.')
-        time.sleep(15)
+            cellname = f'{file_name_split[0]}_env_image_stack'
+    
+    
+            task_pb = ee.batch.Export.image.toDrive(
+                image = image_float,
+                description = cellname,
+                folder = data_folder, #!imagery
+                region = feature,
+                scale = scale,
+                crs = crs,
+                maxPixels = max_pixels
+            )
+            task_pb.start()
+            print(f'Export task started for {cellname}.')
+            time.sleep(5)
+
+    else:
+        index_num = -1
+        for shp_file in shp_files:
+            index_num = index_num + 1
+            file_name_split = shp_file.split(".")
+            
+            shp_path = os.path.join(shp_dir, shp_file)
+            feature_collection = geemap.shp_to_ee(shp_path)
+            feature = feature_collection.geometry()
+            image_path = f'{folder_directory}/{data_folder}/{file_name_split[0]}_env_image.tif'
+            geemap.ee_export_image(image_stack, image_path, scale, region = feature)
+            print(f'Export task started for {shp_file}.')
+            time.sleep(15)
